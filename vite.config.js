6fa03b57cff48ed1ac38d4ea328d2893eba99c74
juanmcortez <juanm.cortez@gmail.com>
2025-08-06 10:54:20 -0300

    🚧 UI Layout / Dashboard landing.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/base_theme.css', 'resources/js/base_theme.js'],
             refresh: true,
         }),
         tailwindcss(),
