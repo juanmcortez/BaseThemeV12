4af7f6430584ded77473305ff26c82c246476bbf
juanmcortez <juanm.cortez@gmail.com>
2025-08-07 12:26:46 -0300

    💄 Theme edits.

--- resources/views/components/layouts/parts/footer.blade.php
+++ resources/views/components/layouts/parts/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="left-mod">
-        {{ __('FOOTER') }}
+        <span><i class="fi fi-rs-copyright"></i> {{ date('Y') . ' - ' . config('app.name') }}</span>
     </div>
     <div class="center-mod">
-        {{ __('FOOTER') }}
+        <span>{{ __('Icons powered by') }} <i class="fi fi-brands-flaticon-1"></i></span>
     </div>
     <div class="right-mod">
-        {{ __('FOOTER') }}
+        <span>{{ __('System powered by') }} <i class="fi fi-brands-php"></i></span>
     </div>
 </footer>
