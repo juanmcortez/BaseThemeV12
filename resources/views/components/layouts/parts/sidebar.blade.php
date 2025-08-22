08790a57bf696c07d052a224df93a69fa7df251c
juanmcortez <juanm.cortez@gmail.com>
2025-08-22 12:40:36 -0300

    🚧 Theme customization.

--- resources/views/components/layouts/parts/sidebar.blade.php
+++ resources/views/components/layouts/parts/sidebar.blade.php
@@ -1,3 +1,34 @@
 <aside>
-    {{ __('SIDEBAR') }}
+    <section>
+        <h1>{{ config('app.name') }}</h1>
+    </section>
+    <section class="main-menu">
+        <nav>
+            <ul>
+                <li>{{ __('Menu') }}</li>
+            </ul>
+        </nav>
+    </section>
+    <section>
+        <nav class="user-menu">
+            <ul>
+                <li>
+                    <i class="fi fi-rr-circle-user"></i>
+                    @if(auth()->user()->full_name)
+                        {!! __('Welcome <strong>:User</strong>!', ['user' => auth()->user()->username]) !!}
+                    @else
+                        {!! __('Welcome <strong>:User</strong>!', ['user' => auth()->user()->username]) !!}
+                    @endif
+                    <ul class="submenu hidden">
+                        <li>{{ __('Menu item') }}</li>
+                        <li>{{ __('Menu item') }}</li>
+                        <li>{{ __('Menu item') }}</li>
+                        <li>{{ __('Menu item') }}</li>
+                        <li>{{ __('Menu item') }}</li>
+                        <li>{{ __('Menu item') }}</li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </section>
 </aside>
