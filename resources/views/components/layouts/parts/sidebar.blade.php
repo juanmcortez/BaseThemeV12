a771c3d0f34bd94cc90ed85e7174c3ba4cb850ae
juanmcortez <juanm.cortez@gmail.com>
2025-08-25 16:50:02 -0300

    🚧 Theme customization.

--- resources/views/components/layouts/parts/sidebar.blade.php
+++ resources/views/components/layouts/parts/sidebar.blade.php
@@ -11,21 +11,38 @@
     </section>
     <section>
         <nav class="user-menu">
-            <ul>
-                <li>
-                    <i class="fi fi-rr-circle-user"></i>
+            <ul x-data="{ open: false }">
+                <li @click="open = !open">
+                    <i class="fi fi-rs-circle-user"></i>
                     @if(auth()->user()->full_name)
-                        {!! __('Welcome <strong>:User</strong>!', ['user' => auth()->user()->username]) !!}
+                        {!! __('Welcome <strong>:User</strong> !!', ['user' => auth()->user()->username]) !!}
                     @else
-                        {!! __('Welcome <strong>:User</strong>!', ['user' => auth()->user()->username]) !!}
+                        {!! __('Welcome <strong>:User</strong> !!', ['user' => auth()->user()->username]) !!}
                     @endif
-                    <ul class="submenu hidden">
+                    <ul class="submenu" x-show="open" @click.away="open = false"
+                        x-transition:enter="transition-opacity ease-out duration-150"
+                        x-transition:enter-start="opacity-0"
+                        x-transition:enter-end="opacity-100"
+                        x-transition:leave="transition-opacity ease-in duration-150"
+                        x-transition:leave-start="opacity-100"
+                        x-transition:leave-end="opacity-0"
+                    >
                         <li>{{ __('Menu item') }}</li>
                         <li>{{ __('Menu item') }}</li>
                         <li>{{ __('Menu item') }}</li>
                         <li>{{ __('Menu item') }}</li>
                         <li>{{ __('Menu item') }}</li>
                         <li>{{ __('Menu item') }}</li>
+                        <li>
+                            @auth
+                                {{-- LOGOUT --}}
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <i class="fi fi-rs-exit"></i> {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST">@csrf</form>
+                                {{-- LOGOUT --}}
+                            @endauth
+                        </li>
                     </ul>
                 </li>
             </ul>
